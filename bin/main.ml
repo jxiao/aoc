afc38770589821e6ef72740826e584bc0c74b645
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2024-08-14 00:33:06 -0400

    Files

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open Aoc.Day1
 
-let day1 = "files/day1/full.txt"
+let day1 = "files/day1.txt"
 let print_result ans = Printf.printf "Day 1, Part 1 = %i\n%!" ans;;
 
 part_one_result day1 |> print_result;;
