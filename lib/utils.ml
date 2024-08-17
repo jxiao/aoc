24cc280d299da295e81edf123a78285db337c46a
Jeffrey Xiao <55001755+jxiao@users.noreply.github.com>
2024-08-17 18:54:52 -0400

    Day 4 (#2)

--- lib/utils.ml
+++ lib/utils.ml
@@ -2,6 +2,11 @@ let print_list f = List.iter (fun v -> f v |> print_endline)
 let char_list_of_string s = s |> String.to_seq |> List.of_seq
 let sum = List.fold_left ( + ) 0
 
+let digit_opt c =
+  match c with
+  | '0' .. '9' -> Some (int_of_char c - int_of_char '0')
+  | _ -> None
+
 let file_lines name =
   let channel = open_in name in
   let read_line () = try Some (input_line channel) with End_of_file -> None in
